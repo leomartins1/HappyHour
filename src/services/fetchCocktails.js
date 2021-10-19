9affef6d77550eda687baffdc5312d45cd44652b
Leo <leonardomartins07@gmail.com>
2021-10-19 14:25:12 -0300

    creating fetch functions

--- src/services/fetchCocktails.js
+++ src/services/fetchCocktails.js
@@ -3,30 +3,38 @@ const Error = 'Sinto muito, não encontramos nenhuma receita para esses filtros.
 export async function fetchDrinksByIngredient(type) {
   try {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${type}`);
-    return await response.json();
+    const response_1 = await response.json();
+    return response_1.drinks;
   } catch (e) {
     return global.alert(Error);
   }
 }
+
 export async function fetchDrinksByName(type) {
-  try {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${type}`);
-    return await response.json();
-  } catch (e) {
-    return global.alert(Error);
-  }
+    const response_1 = await response.json();
+    if(response_1.drinks === null ) return global.alert(Error);
+    return response_1.drinks;
 }
+
 export async function fetchDrinksByFirstLetter(type) {
-  if (type.length > 1) global.alert('Sua busca deve conter somente 1 (um) caracter');
+  if (type.length > 1) return global.alert('Sua busca deve conter somente 1 (um) caracter');
   try {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${type}`);
-    return await response.json();
+    const response_1 = await response.json();
+    return response_1.drinks;
   } catch (e) {
     return global.alert(Error);
   }
 }
-export function fetchInitialDrinks() {
-  return fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-    .then((response) => response.json())
-    .then((response) => response.drinks)
+
+export async function fetchInitialDrinks() {
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+  const response_1 = await response.json();
+  return response_1.drinks;
+}
+
+export async function fetchDrinksDetails(id) {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`);
+  return await response.json();
 }
