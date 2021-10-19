b478f2420a7334baca48495032b9583d86baf1e7
Leo <leonardomartins07@gmail.com>
2021-10-19 14:23:06 -0300

    changing list logic

--- src/components/DrinkList.jsx
+++ src/components/DrinkList.jsx
@@ -1,22 +1,32 @@
 import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { useCallback } from 'react/cjs/react.development';
 import DrinksContext from '../context/DrinksContext';
 
 export default function DrinkList() {
   const { contextValue } = useContext(DrinksContext);
   const { Drinks } = contextValue;
-  return (
-    <div className="home">
-      {Drinks.map((drink) => (
-        <div className="drink-card">
+
+  const handleDrinks = useCallback(() => {
+    if (Drinks === undefined || Drinks === null) return 
+    return (
+      <div className="home">
+      {Drinks.map((drink, index) => (
+        <div className="drink-card" key={ index }>
           <img
             className="img-card"
             src={drink.strDrinkThumb}
             alt={drink.strDrink}
           />
           <p>{drink.strDrink}</p>
-          <p>{drink.idDrink}</p>
+          <Link to={`${drink.idDrink}`}>Detalhes</Link>
         </div>
       ))}
     </div>
+    )
+  }, [Drinks])
+
+  return (
+    <div>{handleDrinks()}</div>
   );
 }
