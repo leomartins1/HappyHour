41cb8cb408ca7a82e5b4ff6721fcc2532afebfd2
Leo <leonardomartins07@gmail.com>
2021-10-19 20:02:35 -0300

    adding category filter

--- src/components/SearchInput.jsx
+++ src/components/SearchInput.jsx
@@ -1,8 +1,7 @@
 import React, { useContext, useState } from 'react';
 import DrinksContext from '../context/DrinksContext';
-import { fetchDrinksByFirstLetter, fetchDrinksByIngredient, fetchDrinksByName } from '../services/fetchCocktails';
-
-const THE_LAST_ONE = 25;
+import { fetchDrinksByCategory, fetchDrinksByFirstLetter, fetchDrinksByIngredient, fetchDrinksByName } from '../services/fetchCocktails';
+import icon from '../img/icon.svg';
 
 export default function SearchInput() {
   const { contextValue } = useContext(DrinksContext);
@@ -27,20 +26,22 @@ export default function SearchInput() {
     switch (filters.query) {
       case 'ingredient': {
         const drinks = await fetchDrinksByIngredient(filters.input)
-        const first24 = drinks.slice(0, THE_LAST_ONE)
-        setDrinks(first24);
+        setDrinks(drinks);
         break;
       }
       case 'name': {
         const drinks = await fetchDrinksByName(filters.input)
-        const first24 = drinks.slice(0, THE_LAST_ONE)
-        setDrinks(first24);
+        setDrinks(drinks);
         break;
       }
       case 'first-letter': {
         const drinks = await fetchDrinksByFirstLetter(filters.input)
-        const first24 = drinks.slice(0, THE_LAST_ONE)
-        setDrinks(first24);
+        setDrinks(drinks);
+        break;
+      }
+      case 'category': {
+        const drinks = await fetchDrinksByCategory(filters.input)
+        setDrinks(drinks);
         break;
       }
       default:
@@ -49,44 +50,48 @@ export default function SearchInput() {
   }
 
   return (
-    <>
-    <input type="text" data-testid="name-filter" onChange={ handleFilter } />
-    <form onChange={ handleChange }>
+    <div className="search">
+      <p>Happy Hour</p>
+      <input className="input" type="text" data-testid="name-filter" onChange={ handleFilter } />
+      <form onChange={ handleChange }>
         <label htmlFor="ingredient">
-          Ingrediente
           <input
             type="radio"
             data-testid="ingredient-search-radio"
             name="search"
             id="ingredient"
           />
+          Ingrediente
         </label>
         <label htmlFor="name">
-          Nome
           <input
             type="radio"
             data-testid="name-search-radio"
             name="search"
             id="name"
           />
+          Nome
+        </label>
+        <label htmlFor="category">
+          <input
+            type="radio"
+            data-testid="category-search-radio"
+            name="search"
+            id="category"
+          />
+          Categoria
         </label>
         <label htmlFor="first-letter">
-          Primeira Letra
           <input
             type="radio"
             data-testid="first-letter-search-radio"
             name="search"
             id="first-letter"
           />
+          Primeira Letra
         </label>
-        </form>
-        <button
-          type="button"
-          data-testid="exec-search-btn"
-          onClick={ handleClick }
-        >
-            Pesquisar
-        </button>
-    </>
+      </form>
+      <img className="btn-src" alt="search-btn" onClick={ handleClick } src={icon}/>
+    </div>
   );
 }
--- src/services/fetchCocktails.js
+++ src/services/fetchCocktails.js
@@ -1,8 +1,9 @@
-const Error = 'Sinto muito, não encontramos nenhuma receita para esses filtros.';
+const Error = 'Sinto muito, não encontramos nenhuma bebida.';
 
 export async function fetchDrinksByIngredient(type) {
   try {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${type}`);
+    console.log(response)
     const response_1 = await response.json();
     return response_1.drinks;
   } catch (e) {
@@ -12,6 +13,7 @@ export async function fetchDrinksByIngredient(type) {
 
 export async function fetchDrinksByName(type) {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${type}`);
+    console.log(response)
     const response_1 = await response.json();
     if(response_1.drinks === null ) return global.alert(Error);
     return response_1.drinks;
@@ -21,6 +23,18 @@ export async function fetchDrinksByFirstLetter(type) {
   if (type.length > 1) return global.alert('Sua busca deve conter somente 1 (um) caracter');
   try {
     const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${type}`);
+    console.log(response)
+    const response_1 = await response.json();
+    return response_1.drinks;
+  } catch (e) {
+    return global.alert(Error);
+  }
+}
+
+export async function fetchDrinksByCategory(type) {
+  try {
+    const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${type.replace(' ', '_')}`);
+    console.log(response)
     const response_1 = await response.json();
     return response_1.drinks;
   } catch (e) {
@@ -30,6 +44,7 @@ export async function fetchDrinksByFirstLetter(type) {
 
 export async function fetchInitialDrinks() {
   const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+  console.log(response)
   const response_1 = await response.json();
   return response_1.drinks;
 }
